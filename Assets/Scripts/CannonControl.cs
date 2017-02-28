47767c1efbf5b9464dbe1137cf68d350d796d25c
Chris Boston <bostonchris322@gmail.com>
2017-02-28 15:56:04 -0500

    working on input...slow going.

--- Assets/Scripts/CannonControl.cs
+++ Assets/Scripts/CannonControl.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 /*
 Left stick (horizontal) to adjust rotation
 Right stick (vertical) to adjust pitch
 right trigger (hold for power adjust) to fire 
+
+
+    joy1-3 are launchers
+    joy 4 is runner
 */
 
-public class CannonControl : MonoBehaviour {
+public class CannonControl : MonoBehaviour
+{
+    public bool _________________;
 
-	// Use this for initialization
-	void Start () {
-		
+    int player1JoyNum = 1;
+    int player2JoyNum = 2;
+    int player3JoyNum = 3;
+    int player4JoyNum = 4;
+
+
+    // Use this for initialization
+    void Start () {
+	    
 	}
 	
 	// Update is called once per frame
@@ -20,3 +33,4 @@ public class CannonControl : MonoBehaviour {
 		
 	}
 }
+
