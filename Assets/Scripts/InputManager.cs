d0e7bf60a6c58beeaf7ecfc280df397bd3767219
Chris Boston <bostonchris322@gmail.com>
2017-02-28 15:22:42 -0500

    adding input scripts

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+Debugger should be able to use PC keyboard/mouse to control each player (one at a time).
+Use number keys 1, 2, 3, 4 to switch control between the respective players.
+ */
+
 public class InputManager : MonoBehaviour {
 
 	// Use this for initialization
