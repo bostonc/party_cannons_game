c9c1ba6541a10e2ba06c53a027ffb566bc69d7a6
Chris Boston <bostonchris322@gmail.com>
2017-03-05 13:44:09 -0500

    cannonball despawner added

---
+++ Assets/Scripts/CannonBallDespawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBallDespawn : MonoBehaviour {
+    public float despawnSpeed = .05f;
+
+    Rigidbody rb;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (gameObject.transform.position.y < -10 || rb.velocity == Vector3.zero ||
+            (rb.velocity.x < despawnSpeed && rb.velocity.y < despawnSpeed && rb.velocity.z < despawnSpeed))
+        {
+            Destroy(gameObject);
+        }
+	}
+}
