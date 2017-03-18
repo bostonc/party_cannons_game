ce66a7678d12a7a9022fda34a3d15a9fe3df59fe
Valliappa Chockalingam <valli@umich.edu>
2017-03-18 00:18:18 -0400

    Changes to RunnerFallSwap script with some comments within to make things a bit more clear about how the InputManager script now works. (No testing done though.)

--- Assets/Scripts/RunnerFallSwap.cs
+++ Assets/Scripts/RunnerFallSwap.cs
@@ -11,13 +11,13 @@ public class RunnerFallSwap : MonoBehaviour
     public GameObject cannon_2;
     public GameObject cannon_3;
 
-    int RunnerPlayerNumber = 0; //1 - 4
-    int lowestScorePlayerNumber = 0; //1 - 4
-	
+	// Prefer use of PlayerID/ControlID enums wherever possible (over integers or other numeric types).
+	InputManager.PlayerID playerWithLowestScore = 0;
+
 	// Update is called once per frame
 	void Update ()
     {
-        positionCheck();		
+        positionCheck();
 	}
 
     void positionCheck()
@@ -32,68 +32,63 @@ public class RunnerFallSwap : MonoBehaviour
         //if runner has fallen
         if (runner.transform.position.y < fallenDetectionDistance)
         {
-            //look through player list for runner
-            for (int i = 0; i < 4; i++)
-            {
-                if (InputManager.S.playerInfoList[i].controlID == InputManager.ControlID.Runner)
-                {
-                    RunnerPlayerNumber = i;
-                }
-            }
             //who has the lowest score?
             if (Scorekeeper.S.p1Score < Scorekeeper.S.p2Score &&
                 Scorekeeper.S.p1Score < Scorekeeper.S.p3Score &&
                 Scorekeeper.S.p1Score < Scorekeeper.S.p4Score)
             {
-                lowestScorePlayerNumber = 1;
+				playerWithLowestScore = InputManager.PlayerID.Player1;
             }
             else if (Scorekeeper.S.p2Score < Scorekeeper.S.p1Score &&
                 Scorekeeper.S.p2Score < Scorekeeper.S.p3Score &&
                 Scorekeeper.S.p2Score < Scorekeeper.S.p4Score)
             {
-                lowestScorePlayerNumber = 2;
+				playerWithLowestScore = InputManager.PlayerID.Player2;
             }
             else if (Scorekeeper.S.p3Score < Scorekeeper.S.p1Score &&
                 Scorekeeper.S.p3Score < Scorekeeper.S.p2Score &&
                 Scorekeeper.S.p3Score < Scorekeeper.S.p4Score)
             {
-                lowestScorePlayerNumber = 3;
+				playerWithLowestScore = InputManager.PlayerID.Player4;
             }
             else if (Scorekeeper.S.p4Score < Scorekeeper.S.p1Score &&
                 Scorekeeper.S.p4Score < Scorekeeper.S.p2Score &&
                 Scorekeeper.S.p4Score < Scorekeeper.S.p3Score)
             {
-                lowestScorePlayerNumber = 4;
+				playerWithLowestScore = InputManager.PlayerID.Player4;
             }
 
-            if (RunnerPlayerNumber == lowestScorePlayerNumber)
+			// All necessary information for a player can be got through a control ID or a player ID by 
+			// using the getPlayerInfoWith*ID (where * = Control or Player). ControlID is one of 
+			// {Cannon1, Cannon2, Cannon3, Runner}. PlayerID is one of {Player1, Player2, Player3, Player4}.
+			// A PlayerInfo object is returned on a call to both of these functions. Objects of this class 
+			// contain a PlayerID, ControlID and an AIMode.
+			if (InputManager.S.getPlayerInfoWithControlID(InputManager.ControlID.Runner).playerID == 
+				playerWithLowestScore)
             {
                 //find second lowest score player
                 int secondLowestScore = scoreList[1];
                 if (secondLowestScore == Scorekeeper.S.p1Score)
                 {
-                    lowestScorePlayerNumber = 1;
+					playerWithLowestScore = InputManager.PlayerID.Player1;
                 }
                 else if (secondLowestScore == Scorekeeper.S.p2Score)
                 {
-                    lowestScorePlayerNumber = 2;
+					playerWithLowestScore = InputManager.PlayerID.Player2;
                 }
                 else if (secondLowestScore == Scorekeeper.S.p3Score)
                 {
-                    lowestScorePlayerNumber = 3;
+					playerWithLowestScore = InputManager.PlayerID.Player3;
                 }
                 else if (secondLowestScore == Scorekeeper.S.p4Score)
                 {
-                    lowestScorePlayerNumber = 4;
+					playerWithLowestScore = InputManager.PlayerID.Player4;
                 }
             }
 
-            //swap
-
-
-
-
-
+            //swap (Get Control ID of player with lowest score. The runner will then take on the cannon 
+			// specified, and the player with the lowest score will now take on the runner role.)
+			InputManager.S.swapPlayer(InputManager.S.getPlayerInfoWithPlayerID(playerWithLowestScore).controlID);
         }
     }
 
