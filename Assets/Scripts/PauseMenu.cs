3dbdacb33876a4d8695c367eea85863bc30f6c8f
Chris Boston <bostonchris322@gmail.com>
2017-04-01 23:19:31 -0400

    pause menu update

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+
+    public void PressResume()
+    {
+        unpause();
+        gameObject.SetActive(false);
+    }
+
+    public void PressRestart()
+    {
+        unpause();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void PressQuit()
+    {
+        unpause();
+        SceneManager.LoadScene("StartUpMenu");
+    }
+
+    private void unpause()
+    {
+        Time.timeScale = 1;
+        InputManager.S.paused = false;
+    }
+}
