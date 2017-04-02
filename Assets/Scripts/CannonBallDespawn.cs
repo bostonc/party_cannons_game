f2827a859932565ebe83102210c60fb2982967ab
Valliappa Chockalingam <valli@umich.edu>
2017-04-02 17:30:34 -0400

    Added somewhat working effect of cannonball hits. Splash damage w/ particles not implemented yet.

--- Assets/Scripts/CannonBallDespawn.cs
+++ Assets/Scripts/CannonBallDespawn.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CannonBallDespawn : MonoBehaviour {
     public float despawnSpeed = .1f;
 
+	bool collisionEffectComplete = false;
+
     Rigidbody rb;
 
 
@@ -17,17 +19,28 @@ public class CannonBallDespawn : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-		if (gameObject.transform.position.y < -10 || (rb.velocity - Vector3.zero).magnitude < 0.2f ||
+		if (gameObject.GetComponent<ParticleSystem> ().main.duration - gameObject.GetComponent<ParticleSystem> ().time > 0)
+			return;
+		else if (collisionEffectComplete || // Collision Effect Done + Duration Over, destory cannonball.
+			gameObject.transform.position.y < -10 || (rb.velocity - Vector3.zero).magnitude < 0.2f ||
 			(Mathf.Abs(rb.velocity.x) < 2e2*despawnSpeed && Mathf.Abs(rb.velocity.y) < 2e2*despawnSpeed && 
-				Mathf.Abs(rb.velocity.z) < 1e-5*despawnSpeed))
-        {
-            Destroy(gameObject);
-        }
+				Mathf.Abs(rb.velocity.z) < 1e-5*despawnSpeed))  // + Safety Check Condition
+		{
+			Destroy(gameObject);
+		}
 	}
 
 	public void OnCollisionEnter(Collision coll) {
-		if (coll.gameObject.name == "Backdrop") {
-
+		if (collisionEffectComplete)
+			return;
+		if (coll.gameObject.name == "Backdrop" || coll.gameObject.name == "Runner" || 
+			coll.gameObject.name.Contains("Platform") || (coll.gameObject.transform.parent != null && 
+			coll.gameObject.transform.parent.name == "Map")) {
+			ParticleSystem ps = gameObject.GetComponent<ParticleSystem> ();
+			ParticleSystem.EmissionModule em = ps.emission;
+			em.enabled = true;
+			ps.Play ();
+			collisionEffectComplete = true;
 		}
 	}
 }
