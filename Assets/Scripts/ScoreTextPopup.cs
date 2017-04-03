bfe57f84fb186670f21030e9dad1ffc9b53262d5
Chris Boston <bostonchris322@gmail.com>
2017-04-02 21:06:16 -0400

    message popup framework in place

--- Assets/Scripts/ScoreTextPopup.cs
+++ Assets/Scripts/ScoreTextPopup.cs
@@ -6,13 +6,14 @@ public class ScoreTextPopup : MonoBehaviour
 {
     public int minFont = 0;
     public int maxFont = 20;
-    public float FontSizeTransitionSpeed = 5;
-    public float riseSpeed = .2f; //speed of text rise
+    public float fontTransitionSpeed = 1;
+    public float riseSpeed = .1f; //speed of text rise
     public float fadeDelay = 2; //seconds before fade
     public float fadeSpeed = .01f;
     public string msg = "";
     Vector3 defaultSpawnLoc = new Vector3(0f, 10f, 24f);
     TextMesh tm;
+    float currentFontSize = 0;
 
     float spawnTime;
 
@@ -21,6 +22,8 @@ public class ScoreTextPopup : MonoBehaviour
     {
         tm = gameObject.GetComponent<TextMesh>();
         spawnTime = Time.time;
+        tm.fontSize = minFont;
+        currentFontSize = minFont;
 	}
 	
 	// Update is called once per frame
@@ -33,22 +36,37 @@ public class ScoreTextPopup : MonoBehaviour
 
     void fade()
     {
-
+        if (!(Time.time - spawnTime > fadeDelay)) return;
+        Color c = tm.color;
+        c.a = c.a - fadeSpeed;
+        tm.color = c;
+        if (tm.color.a <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void rise()
     {
-
+        gameObject.transform.position = new Vector3(
+            gameObject.transform.position.x,
+            gameObject.transform.position.y + riseSpeed,
+            gameObject.transform.position.z);
     }
 
     void fontTransform()
     {
-
+        if (currentFontSize < maxFont)
+        {
+            currentFontSize += fontTransitionSpeed;
+            tm.fontSize = (int)currentFontSize;
+        }
     }
 
     public void construct(string msg, Vector3 loc)
     {
-
+        gameObject.transform.position = loc;
+        tm.text = msg;
     }
 
 }
