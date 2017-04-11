aea94a3309d807a906092a85d67e1cb5ab7bc199
Chris Boston <bostonchris322@gmail.com>
2017-04-11 19:44:21 -0400

    added trailer scene

---
+++ Assets/Scripts/Trailer_CatMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trailer_CatMovement : MonoBehaviour
+{
+    float speed = .1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 oldOPos = gameObject.transform.position;
+        Vector3 pos = new Vector3(oldOPos.x + speed, oldOPos.y, oldOPos.z);
+        gameObject.transform.position = pos;
+    }
+}
